915bfda9aff692a07cf5360a538220e535eef29d
Michael Scott <michaelcscott19871@gmail.com>
2016-12-14 05:54:36 -0500

    Add files via upload

---
+++ ConsoleApplication7/FindWords.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionary
+{
+    // Class for words and functions
+    class FindWords
+    {
+        // Function to get Highest Word Count
+        public int MaxCount(Dictionary<String, int> list)
+        {
+            int max = 0;
+            foreach (var key in list.Keys.ToList<String>()) // Comparing every count in the list to find maximum count
+            {
+                if (max < list[key])
+                {
+                    max = list[key];
+                }
+            }
+            return max;
+        }
+        // Function to get Average Word Count
+        public double AverageCount(Dictionary<String, int> list)
+        {
+            double sum = 0;
+            List<String> keys = list.Keys.ToList<String>();
+            foreach (var key in keys)                      // Adding all counts of word to get average
+            {
+                sum += list[key];
+            }
+            return sum / keys.Capacity;                    // Calculating average
+        }
+        // Function to get Words w.r.t length
+        public List<String> WordLength(Dictionary<String, int> list, int length)
+        {
+            List<String> words = new List<String>();
+            foreach (var key in list.Keys.ToList<String>()) // Finding all words in the list that have same length to parameter length
+            {
+                if (key.Length == length)                   
+                {
+                    words.Add(key);
+                }
+            }
+            words.Sort();                                   // Sorting all words
+            return words;
+        }
+        // Function to get Word w.r.t Count
+        public List<String> WordCount(Dictionary<String, int> list, int count)
+        {
+            List<String> words = new List<String>();
+            foreach (var key in list.Keys.ToList<String>())
+            {
+                if (list[key] == count)                     // Finding all words in the list that have the same count as given in parameter
+                {
+                    words.Add(key);
+                }
+            }
+            words.Sort();                                   // Sorting words
+            return words;
+        }
+    }
+}
---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dictionary
+{
+    class Program
+    {
+        // Main function
+        static void Main(string[] args)
+        {
+            FindWords word_obj = new FindWords();       // Object to call functions from FindWords class
+            string fileName;                            // Variable for file name
+            Console.Write("Enter file name: ");         
+            fileName = Console.ReadLine();
+            // Making Stream Reader object file will be placed in the same directory as code hence we used "../../"
+            StreamReader sr = new StreamReader("../../" + fileName);        
+            Dictionary<String, int> word_list = new Dictionary<string, int>();  // List to save words
+            while (!sr.EndOfStream)                     // Loop to read file until end
+            {
+                String[] words = sr.ReadLine().Split(' ');  // Copying words to an array and spliting them using spaces
+                foreach (var word in words)                 // Adding all words in the list
+                {
+                    if (word_list.ContainsKey(word))
+                    {
+                        word_list[word] = word_list[word] + 1;
+                    }
+                    else
+                    {
+                        word_list[word] = 1;
+                    }
+                }
+            }
+            String dict_word;
+            int option, length, count;
+            while (true)                // Loop to get user option
+            {
+                Console.WriteLine("\n1. Highest word count");
+                Console.WriteLine("2. Average word count");
+                Console.WriteLine("3. Number of times a word is used");
+                Console.WriteLine("4. Get Words based on count");
+                Console.WriteLine("5. Get Words based on length");
+                Console.WriteLine("6. Exit");
+                Console.Write("Choose an option: ");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)     // Switch statement
+                {
+                    case 1:         // If user chooses option 1
+                        Console.WriteLine("Higest Word Count : " + word_obj.MaxCount(word_list));
+                        break;
+                    case 2:         // If user chooses option 2
+                        Console.WriteLine("Average Word Count : " + word_obj.AverageCount(word_list));
+                        break;
+                    case 3:         // If user chooses option 3
+                        Console.Write("Enter a word: ");
+                        dict_word = Console.ReadLine();
+                        if (word_list.ContainsKey(dict_word))
+                        {
+                            Console.WriteLine(dict_word + " is used " + word_list[dict_word] + " times");
+                        }
+                        else
+                        {
+                            Console.WriteLine(dict_word + " is never used");
+                        }
+                        break;
+                    case 4:         // If user chooses option 4
+                        Console.Write("Enter count: ");
+                        count = Convert.ToInt32(Console.ReadLine());
+                        foreach (var word in word_obj.WordCount(word_list, count))
+                        {
+                            Console.WriteLine(word);
+                        }
+                        break;
+                    case 5:         // If user chooses option 5
+                        Console.Write("Enter length: ");
+                        length = Convert.ToInt32(Console.ReadLine());
+                        foreach (var word in word_obj.WordLength(word_list, length))
+                        {
+                            Console.WriteLine(word);
+                        }
+                        break;
+                    case 6:
+                        return;
+                    default:        // If user chooses invalid option
+                        Console.WriteLine("Choice Invalid!!. Please Choose again");
+                        break;
+                }
+            }
+        }
+    }
+}
